b0fe05459196e23358da4498930b30ae915940de
Michael <michaelkatom10@gmail.com>
2020-11-28 16:31:48 +0200

    added server folder

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import express, { Application } from "express";
+
+const app: Application = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/check", (req, res) => {
+  return res.send(200);
+});
+
+app.listen(5000, () => {
+  console.log("app listening on port 5000");
+});
