4d9b5a4f19a68dac5b006ba6764063dff586826b
Dov <dovbor@gmail.com>
2020-11-29 21:34:35 +0200

    add post interface

--- src/interfaces/post.interface.ts
+++ src/interfaces/post.interface.ts
@@ -1,1 +1,10 @@
-export interface Post {}
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface IPost extends Document {
+  _id: mongoose.Types.ObjectId,
+  publisher: string;
+  trainingID: Schema.Types.ObjectId;
+  content: string;
+  numOfLikes: number;
+  date: Schema.Types.Date;
+}
