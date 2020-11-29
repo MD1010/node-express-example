f030b0b2f7b9759a9f5e266a2487f4284dd116ca
Dov <dovbor@gmail.com>
2020-11-29 21:35:48 +0200

    add posts crud routes

--- src/routes/post.route.ts
+++ src/routes/post.route.ts
@@ -2,3 +2,9 @@ import { Router } from "express";
 import { PostController } from "../controllers";
 
 export const postRouter = Router();
+
+postRouter.post('/', PostController.createPost);
+postRouter.put('/:id', PostController.updatePost);
+postRouter.get('/:id', PostController.getPost);
+postRouter.delete('/:id', PostController.deletePost);
+postRouter.get('/', PostController.getAllPosts);
