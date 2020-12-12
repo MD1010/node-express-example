5619ebb46375253a83cdaa09f1a873f979d7e215
Jenia <yevgeniyg10@gmail.com>
2020-12-13 00:40:56 +0200

    changed video schema from list to string

--- src/entities/training.entity.ts
+++ src/entities/training.entity.ts
@@ -6,7 +6,7 @@ const TrainingSchema: Schema = new Schema({
   name: { type: String, required: true },
   tags: [{ type: Schema.Types.ObjectId, required: true, ref: "Tag" }],
   exercises: [{ type: Schema.Types.ObjectId, required: true, ref: "Exercise" }],
-  video: [{ type: String, required: true }],
+  video: { type: String, required: true },
   createdAt: { type: Schema.Types.Date, required: true },
   numOfLikes: { type: Number, required: true },
 });
