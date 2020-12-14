b96095e43a0981dc8a6be69fe6235f7471c095e5
Jenia <yevgeniyg10@gmail.com>
2020-12-15 01:42:00 +0200

    start writing generate exercise code

--- src/controllers/exercise.controller.ts
+++ src/controllers/exercise.controller.ts
@@ -1,4 +1,6 @@
+import Axios from "axios";
 import { Request, Response } from "express";
+import { JSDOM } from "jsdom";
 import { threadId } from "worker_threads";
 import { ExerciseDAL } from "../dal/exercise.dal";
 import { ExerciseEntity } from "../entities";
@@ -6,6 +8,7 @@ import { Exercise } from "../models";
 import { Exceptions } from "../utils";
 import { errorHandler } from "../utils/errorHandler";
 import { socketServer } from "../utils/socketManager";
+import { generateExercise } from "./utils/generate-exercise";
 import { GenericCrudController } from "./utils/generic-crud.controller";
 
 export class ExerciseController extends GenericCrudController<Exercise> {
@@ -29,5 +32,29 @@ export class ExerciseController extends GenericCrudController<Exercise> {
 
   groupByTags = errorHandler(async (req: Request, res: Response) => {
     return res.json(await ExerciseDAL.ExericesGroupByTags());
+  });
+
+  scrapExercise = errorHandler(async (req: Request, res: Response) => {
+    let doc: any;
+
+    Axios.get(
+      "https://www.liftlearngrow.com/blog-page/100-best-exercises-list"
+    ).then((res) => {
+      const dom = new JSDOM(res.data);
+
+      let exerciseNames: string[] = [];
+      let exerciseVideos: string[] = [];
+      dom.window.document
+        .querySelectorAll("#block-72839eccdc60de5038fa > div > ul > li > p > a")
+        .forEach((item: any) => exerciseNames.push(item.textContent));
+
+      dom.window.document
+        .querySelectorAll("#block-72839eccdc60de5038fa > div > ul > li > p > a")
+        .forEach((item: any) => exerciseVideos.push(item.href));
+
+      generateExercise(exerciseNames, exerciseVideos);
+    });
+
+    res.json(200);
   });
 }
---
+++ src/controllers/utils/generate-exercise.ts
@@ -0,0 +1,20 @@
+import { exerciseRouter } from "../../routes";
+import { ExerciseDifficulty } from "gymstagram-common";
+
+export const generateExercise = (names: string[], videos: string[]) => {
+  let musclesMap = {
+    chest: 16,
+    back: 35,
+    Legs: 59,
+    shoulders: 71,
+    Bicep: 80,
+    Triceps: 88,
+    Ab: 99,
+  };
+  let setsOptions = [2, 3, 4];
+  let repsOptions = [6, 8, 10, 12, 15];
+  let restTimeOptions = [30, 45, 60, 75, 90];
+  let lenght = names.length;
+
+  for (let index = 0; index < length; index++) {}
+};
