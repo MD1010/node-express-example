e715275e80c5cc493b87c96ba0903aae91b47c5c
Jenia <yevgeniyg10@gmail.com>
2020-12-16 15:48:18 +0200

    change exercise schema

--- src/entities/exercise.entity.ts
+++ src/entities/exercise.entity.ts
@@ -4,11 +4,8 @@ import { Exercise } from "../models";
 
 const ExerciseSchema: Schema = new Schema({
   name: { type: String, required: true },
-  description: { type: String },
   video: { type: String, required: true },
-  muscles: [{ type: mongoose.Types.ObjectId, required: true, ref: "Muscle" }],
   difficulty: { type: Schema.Types.Number, required: true },
-  notes: { type: Schema.Types.String },
   sets: { type: Schema.Types.Number, required: true },
   reps: { type: Schema.Types.Number, required: true },
   restTime: { type: Schema.Types.Number, required: true },
