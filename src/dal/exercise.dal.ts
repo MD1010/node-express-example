1a1dbd3088bbed0917ae8e32e9e386687444727b
Dov <dovbor@gmail.com>
2020-12-22 21:51:20 +0200

    change to _id in group by muscles in exercises

--- src/dal/exercise.dal.ts
+++ src/dal/exercise.dal.ts
@@ -105,7 +105,7 @@ export namespace ExerciseDAL {
             group: { $first: "$muscleGroup.name" },
             excerices: {
               $push: {
-                id: "$_id",
+                _id: "$_id",
                 name: "$name",
                 video: "$video",
                 muscles: "$muscles",
