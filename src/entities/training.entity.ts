4e0bdc29128f11f08326071f4347eef994c463b5
Michael <michaelkatom10@gmail.com>
2021-01-01 23:32:50 +0200

    fixed auth

--- src/entities/training.entity.ts
+++ src/entities/training.entity.ts
@@ -14,7 +14,7 @@ const TrainingSchema: Schema = new Schema(
     numOfLikes: { type: Number, default: 0 },
     likedBy: [{ type: String, default: [] }],
     difficultyLevel: { type: Number, required: true },
-    duration: { type: Number, required: true },
+    duration: { type: Number },
   },
   { versionKey: false }
 );
