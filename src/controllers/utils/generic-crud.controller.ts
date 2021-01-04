4f770ca83244709902ea6beb7e2794c7ebb73a98
Dov <dovbor@gmail.com>
2021-01-04 20:47:28 +0200

    add musclesGroups and duration filter for training

--- src/controllers/utils/generic-crud.controller.ts
+++ src/controllers/utils/generic-crud.controller.ts
@@ -9,36 +9,9 @@ export abstract class GenericCrudController<T extends Document> {
 
   protected getEntities = errorHandler(async (req: Request, res: Response) => {
     let { pageNumber, ...filter } = req.query;
-    let customFilter1 = {};
-    let customFilter2 = {};
-    let customFilter3 = {};
-    let customFilter4 = {};
+    let { name, muscles, muscleGroup, musclesGroups, duration, difficultyLevel, ...remainingFilters } = filter;
 
-    let { name, muscles, muscleGroup, difficultyLevel, ...remainingFilters } = filter;
-
-    if (filter.name) {
-      customFilter3 = { name: new RegExp([filter.name].join(""), "i") as any };
-      delete filter.name;
-    }
-    if (filter.muscles) {
-      let arr = filter.muscles.toString().split(",");
-      customFilter1 = { $or: [{ "muscles.primary": { $in: arr } }, { "muscles.secondary": { $in: arr } }] };
-      delete filter.muscles;
-    }
-
-    if (filter.muscleGroup) {
-      let muscleGroup = filter.muscleGroup.toString();
-      customFilter2 = { muscleGroup: muscleGroup };
-      delete filter.muscleGroup;
-    }
-
-    if (filter.difficultyLevel) {
-      let difficultyLevel = filter.difficultyLevel.toString();
-      customFilter4 = { difficultyLevel: difficultyLevel };
-      delete filter.difficultyLevel;
-    }
-
-    const filter23 = {
+    const customFilter = {
       $and: [
         !isEmpty(name) ? { name: new RegExp([name].join(""), "i") as any } : {},
         !isEmpty(muscles)
@@ -51,21 +24,14 @@ export abstract class GenericCrudController<T extends Document> {
           : {},
         !isEmpty(muscleGroup) ? { muscleGroup: muscleGroup?.toString() } : {},
         !isEmpty(difficultyLevel) ? { difficultyLevel: difficultyLevel?.toString() } : {},
+        !isEmpty(musclesGroups) ? { musclesGroups: { $in: musclesGroups?.toString() } } : {},
+        !isEmpty(duration) ? { duration: { $lte: duration } } : {},
         { ...remainingFilters },
       ],
     };
 
-    const finalFilter = {
-      $and: [{ ...customFilter3 }, { ...customFilter1 }, { ...customFilter2 }, { ...customFilter4 }, { ...filter }],
-    };
-    console.log(finalFilter);
-
-    console.log("-----------------------------------------------------------------");
-    console.log(filter23);
-    const entities = await this.dbEntity.find(finalFilter, pageNumber?.toString());
-
-    const totalFilteredRecords = (await this.dbEntity.find(finalFilter)).length;
-    // const totalFilteredRecords = (await this.dbEntity.getModel().find(filter23)).length;
+    const entities = await this.dbEntity.find(customFilter, pageNumber?.toString());
+    const totalFilteredRecords = (await this.dbEntity.find(customFilter)).length;
 
     return res.json({ entities, totalFilteredRecords });
   });
